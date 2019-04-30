f267f12143980561b139580b6891f0a9345b10b9
Jesse Op de Beeck <42997689+jesse-opdebeeck@users.noreply.github.com>
2019-04-30 13:48:23 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php $title="Home"; include "partials/head.php" ?>
+<header>
+  <div class="overlay"></div>
+  <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+    <source src="https://app.coverr.co/s3/mp4/Scroll-It.mp4" type="video/mp4">
+  </video>
+  <div class="container h-100">
+    <div class="d-flex h-100 text-center align-items-center">
+      <div class="w-100 text-white">
+        <h1 class="display-3">Jesse Op de Beeck</h1>
+        <p class="lead mb-0">Goed met websites en computers en zo van die dingen.</p>
+      </div>
+    </div>
+  </div>
+</header>
+
+<section class="my-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 mx-auto text-center">
+      <h1 class="pageheader">In constructie</h1>
+        <nav class="nav justify-content-center">
+          <li class="nav-item">
+            <a href="#" class="nav-link display-4">
+              Skills
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link display-4">
+              Projecten
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link display-4">
+              Blog
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link display-4">
+              Contact
+            </a>
+          </li>
+        </nav>
+        
+      </div>
+    </div>
+  </div>
+</section>
---
+++ partials/foot.php
@@ -0,0 +1,7 @@
+<footer class="copyright-footer">
+  <div class="text-center">
+    <small>Copyright &copy; Jesse Op de Beeck, 2019</small>
+  </div>
+</footer>
+</body>
+</html>
